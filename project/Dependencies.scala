1644aaf84bcb16480b95af3cd4079eb13ebee4c6
Adam Bridge <bridge.adam@gmail.com>
2025-07-07 17:01:05 -0400

    Bump versions for kafka 3.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,10 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val Scala3    = "3.3.4"
-    val Scala213  = "2.13.15"
+    val Scala3    = "3.3.6"
+    val Scala213  = "2.13.16"
     val Scala212  = "2.12.20"
-    val Kafka     = "3.9.0"
+    val Kafka     = "3.9.1"
     val Slf4j     = "1.7.36"
     val ScalaTest = "3.2.19"
   }
